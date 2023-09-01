d976334ee1d23ace15ced38c5ccfd842ecebd482
Ogie Eweka <oieconsulting23@gmail.com>
2023-08-31 23:58:46 -0400

    added app and db subnet

--- vnet/main.tf
+++ vnet/main.tf
@@ -18,20 +18,20 @@ resource "azurerm_subnet" "public" {
   address_prefixes     = var.public_subnet
 }
 
-# resource "azurerm_subnet" "app" {
-#   count = length(var.app_subnet)
-#   name                 = "${var.name}-${var.env}-app-${count.index}"
-#   resource_group_name  = azurerm_resource_group.rg.name
-#   virtual_network_name = azurerm_virtual_network.vnet.name
-#   address_prefixes     = var.app_subnet[count.index]
-# }
+resource "azurerm_subnet" "app" {
+  count = 2
+  name                 = "${var.name}-${var.env}-app-${count.index}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.app_subnet
+}
 
-# resource "azurerm_subnet" "db" {
-#   count = length(var.db_subnet)
-#   name                 = "${var.name}-${var.env}-db-${count.index}"
-#   resource_group_name  = azurerm_resource_group.rg.name
-#   virtual_network_name = azurerm_virtual_network.vnet.name
-#   address_prefixes     = var.db_subnet[count.index]
-# }
+resource "azurerm_subnet" "db" {
+  count = 2
+  name                 = "${var.name}-${var.env}-db-${count.index}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.db_subnet
+}
 
 
