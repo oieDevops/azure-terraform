838ecad287741b5469c91299b8609f619b3c419d
Ogie Eweka <oieconsulting23@gmail.com>
2023-09-19 17:37:34 +0100

    created dir for latest tf code for hub and spoke network

---
+++ vm/var.tf
@@ -0,0 +1,29 @@
+variable "name" {
+  type = string
+  default = "oie"
+}
+
+# variable "region" {
+#   default = "East US"
+#   type = string
+# }
+
+# variable "" {
+#   type = string
+#   default = ""
+# }
+
+# variable "" {
+#   type = string
+#   default = ""
+# }
+
+# variable "" {
+#   type = string
+#   default = ""
+# }
+
+# variable "" {
+#   type = string
+#   default = ""
+# }
